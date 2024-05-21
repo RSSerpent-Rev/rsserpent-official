590d2377ed7751b61ca62b22072be3f557d34998
EkkoG <beijiu572@gmail.com>
2024-05-21 21:14:23 +0800

    feat: get atom url with md5

--- plugins/rsserpent-plugin-cocoapods/rsserpent_plugin_cocoapods/route.py
+++ plugins/rsserpent-plugin-cocoapods/rsserpent_plugin_cocoapods/route.py
@@ -1,53 +1,33 @@
 from typing import Any, Dict
 
 import arrow
-from rsserpent.utils import HTTPClient, cached
-from lxml import html
+from rsserpent.utils import cached
 import feedparser
+from starlette.exceptions import HTTPException
+import hashlib
 
 
 path = "/cocoapods/{pod}"
 
 
-# @cached
+@cached
 async def provider(pod: str) -> Dict[str, Any]:
     """Define a basic example data provider function."""
-    url = f"https://cocoapods.org/pods/{pod}"
-    async with HTTPClient() as client:
-        pod_resp = await client.get(url)
-        if pod_resp.status_code != 200:
-            feed = feedparser.parse("https://github.com/CocoaPods/Specs/commits/master.atom")
-            items = list(filter(lambda x: pod in x.title, feed.entries))
-            return {
-                "title": f"{pod} Changelog",
-                "link": feed.feed.link,
-                "description": feed.feed.title,
-                "items": list(map(lambda x: {
-                    "title": x.title,
-                    "description": x.content[0].value,
-                    "link": x.link,
-                    "pub_date": arrow.get(x.updated),
-                }, items))
-            }
+    md5 = hashlib.md5(pod.encode()).hexdigest()
+    commit_atom = f"https://github.com/CocoaPods/Specs/commits/master/Specs/{md5[0]}/{md5[1]}/{md5[2]}/{pod}.atom"
+    print(commit_atom)
+    feed = feedparser.parse(commit_atom)
+    if not feed.entries:
+        raise HTTPException(status_code=404, detail="Pod not found")
 
-        else:
-            html_text = pod_resp.content.decode("utf-8")
-            tree = html.fromstring(html_text)
-            # https://github.com/CocoaPods/Specs/blob/master/Specs/5/9/a/Google-Mobile-Ads-SDK/11.5.0/Google-Mobile-Ads-SDK.podspec.json
-            spec_link = tree.xpath("//ul[@class='links']")[0].xpath(".//li")[0].xpath(".//a")[0].attrib["href"]
-            # https://github.com/CocoaPods/Specs/commits/master/Specs/5/9/a/Google-Mobile-Ads-SDK.atom
-            commit_atom = '/'.join(spec_link.split('/')[:12]) + '.atom'
-            commit_atom = commit_atom.replace('blob/', 'commits/')
-            feed = feedparser.parse(commit_atom)
-
-            return {
-                "title": f"{pod} Changelog",
-                "link": feed.feed.link,
-                "description": feed.feed.title,
-                "items": list(map(lambda x: {
-                    "title": x.title,
-                    "description": x.content[0].value,
-                    "link": x.link,
-                    "pub_date": arrow.get(x.updated),
-                }, feed.entries))
-            }
+    return {
+        "title": f"{pod} Changelog",
+        "link": feed.feed.link,
+        "description": feed.feed.title,
+        "items": list(map(lambda x: {
+            "title": x.title,
+            "description": x.content[0].value,
+            "link": x.link,
+            "pub_date": arrow.get(x.updated),
+        }, feed.entries))
+    }
