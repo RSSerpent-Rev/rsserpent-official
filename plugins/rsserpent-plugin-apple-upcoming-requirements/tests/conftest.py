896d600f3179f00345bba7b18ba35ebb5ee91384
EkkoG <beijiu572@gmail.com>
2024-05-22 17:59:00 +0800

    chore: move rsserpent-plugin-apple-upcoming-requirements to seperate repo

--- plugins/rsserpent-plugin-apple-upcoming-requirements/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from rsserpent import app
-from starlette.testclient import TestClient
-
-
-@pytest.fixture(scope="module")
-def client() -> TestClient:
-    """Share one test client across the whole module with `pytest.fixture`."""
-    return TestClient(app)
